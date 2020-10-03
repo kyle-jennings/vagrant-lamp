8ab6f0c37a106a6d07e403dba1c89cf15478b13f
economic-policy-institute <web@epi.org>
2020-10-03 11:58:55 -0400

    added php version info to sidebar

--- www/default/dashboard/blocks/quick-info.php
+++ www/default/dashboard/blocks/quick-info.php
@@ -1,17 +1,18 @@
 <?php
 $specs = [
-    'Public IP' => $_SERVER['SERVER_ADDR'],
-    'Main Address' => $_SERVER['SERVER_NAME'],
-    'Server' => $_SERVER['SERVER_SOFTWARE'],
+    'Public IP'     => $_SERVER['SERVER_ADDR'],
+    'Main Address'  => $_SERVER['SERVER_NAME'],
+    'Server'        => $_SERVER['SERVER_SOFTWARE'],
     'Document Root' => str_replace('/default', '', $_SERVER['DOCUMENT_ROOT']),
-    'HTTP Port' => $_SERVER['SERVER_PORT'],
+    'HTTP Port'     => $_SERVER['SERVER_PORT'],
+    'php version'   => explode('-', phpversion())[0],
 ];
 ?>
 <h3>Quick Info</h3>
 <ul class="list-group">
 <?php
 foreach ($specs as $label => $spec) {
-    $str = $label . ': ' . $spec;
+    $str = '<b>' . $label . '</b>: ' . $spec;
     echo '<li class="list-group-item">' . $str . '</li>';
 }
 ?>
